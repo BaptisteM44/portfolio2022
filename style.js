25141fad11bfa3caaaf4e1f6996dd6a672901a22
BaptisteM44 <bapmorvan@gmail.com>
2022-04-14 14:54:06 +0200

    end JS opacity/contact

--- style.js
+++ style.js
@@ -18,36 +18,51 @@ window.addEventListener('scroll', function(){
     const scroll = this.document.documentElement.scrollTop;
     console.log(scroll);
     if (scroll > 100){
-        // navbartitle.style.padding = "12px"
-        // navbartitle.style.fontSize = "14px"
         if (scroll > 1300){
             img1.style.opacity = "1"
             img2.style.opacity = "1"
+            img8.style.opacity = "1"
         } else {
-            img1.style.opacity = "0.2"
-            img2.style.opacity = "0.2"
+            img1.style.opacity = "0.1"
+            img2.style.opacity = "0.1"
+            img9.style.opacity = "0.1"
         }
         if (scroll > 2400){
             img3.style.opacity = "1"
         } else {
-            img3.style.opacity = "0.2"
+            img3.style.opacity = "0.1"
         }
         if (scroll > 4500){
             img4.style.opacity = "1"
         } else   {
-            img4.style.opacity = "0.2"
+            img4.style.opacity = "0.1"
         }
         if (scroll > 5600){
             img5.style.opacity = "1"
+            img9.style.opacity = "1"
         } else   {
-            img5.style.opacity = "0.2"
+            img5.style.opacity = "0.1"
+            img9.style.opacity = "0.1"
+        }
+        if (scroll > 6700){
+            img6.style.opacity = "1"
+            img7.style.opacity = "1"
+            img5.style.opacity = "0.1"
+            img3.style.opacity = "0.1"
+        } else   {
+            img6.style.opacity = "0.1"
+            img7.style.opacity = "0.1"
+        }
+        if (scroll > 7700){
+            img3.style.opacity = "1"
+            img5.style.opacity = "0.1"
+            img6.style.opacity = "0.1"
+            img7.style.opacity = "0.1"
+            img9.style.opacity = "0.1"
+        } else {
         }
 
-    } else {
-        img3.style.opacity = "0.2"
-        // navbartitle.style.padding = "35px"
-        // navbartitle.style.fontSize = "35px"
-    }
+    } 
     if (scroll > 900){
         img1
     }
