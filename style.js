60e6981ac647569807c241faff0a8c82cc738b92
BaptisteM44 <bapmorvan@gmail.com>
2022-04-15 17:06:11 +0200

    anim name

--- style.js
+++ style.js
@@ -8,8 +8,8 @@ function toggleMenu () {
     });    
   }
   toggleMenu();
-  
-test = document.getElementById("test");
+// END burger
+
 navbartitle = document.getElementById('navbar_title')
 document.getElementById('img1')
 document.getElementById('img2')
@@ -19,11 +19,20 @@ window.addEventListener('scroll', function(){
     const scroll = this.document.documentElement.scrollTop;
     console.log(scroll);
     if (scroll > 100){
+        navbar_title.style.transform = "translate(-100px, 30%)rotate(270deg)"
+        // navbar_title.style.top = "50px"
+        // navbar_title.style.left = "-80px"
+    } else{
+        navbar_title.style.transform = "rotate(0deg)"
+        // navbar_title.style.top = "auto"
+        // navbar_title.style.left = "auto"
+    }
         if (scroll > 1300){
             img1.style.opacity = "1"
             img2.style.opacity = "1"
             img8.style.opacity = "1"
         } else {
+            navbar_title.style.opacity = "1"
             img1.style.opacity = "0.1"
             img2.style.opacity = "0.1"
             img9.style.opacity = "0.1"
@@ -45,7 +54,7 @@ window.addEventListener('scroll', function(){
             img5.style.opacity = "0.1"
             img9.style.opacity = "0.1"
         }
-        if (scroll > 6700){
+        if (scroll > 6300){
             img6.style.opacity = "1"
             img7.style.opacity = "1"
             img5.style.opacity = "0.1"
@@ -54,18 +63,13 @@ window.addEventListener('scroll', function(){
             img6.style.opacity = "0.1"
             img7.style.opacity = "0.1"
         }
-        if (scroll > 7700){
+        if (scroll > 7300){
             img3.style.opacity = "1"
             img5.style.opacity = "0.1"
             img6.style.opacity = "0.1"
             img7.style.opacity = "0.1"
             img9.style.opacity = "0.1"
         } else {
+            
         }
-
-    } 
-    if (scroll > 900){
-        img1
-    }
-
 })
